640f77020ed1090b2f2b58dbaa35c3431b51eb66
Vyacheslav-Lapin <Vyacheslav.Lapin@gmail.com>
2021-07-06 10:13:38 +0300

    Initial commit

---
+++ src/main/java/ru/ais/courses/java/ws/wsdlsoapcontractfirstdemo/controller/Calculator.java
@@ -0,0 +1,2 @@
+package ru.ais.courses.java.ws.wsdlsoapcontractfirstdemo.controller;public class Calculator {
+}
---
+++ src/test/java/ru/ais/courses/java/ws/wsdlsoapcontractfirstdemo/WsdlSoapContractFirstDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.ais.courses.java.ws.wsdlsoapcontractfirstdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdlSoapContractFirstDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
